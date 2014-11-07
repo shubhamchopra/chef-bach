5e6ad4d331590268ab5f356edd94d66e9b45696f
Clay Baenziger <cbaenziger@bloomberg.net>
2014-11-07 10:29:53 -0500

    Standardize copyrights

--- cookbooks/bcpc_hadoop/recipes/revelytix.rb
+++ cookbooks/bcpc_hadoop/recipes/revelytix.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: bcpc_hadoop
+# Recipe:: revelytix
+#
+# Copyright 2014, Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 ruby_block "initialize-revelytix-config" do
     block do
       make_config('revelytix_loom_ssl_password', secure_password)
