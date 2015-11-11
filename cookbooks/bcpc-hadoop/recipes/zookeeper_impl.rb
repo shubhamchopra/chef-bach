c0a5d38af04290f0d844b5719793e52af6976712
Clay Baenziger <cwb@clayb.net>
2015-11-11 03:58:20 -0500

    Test-Kitchen and ServerSpec test updates for ZK (ServerSpec failing due to Ohai gem issue)

--- cookbooks/bcpc-hadoop/recipes/zookeeper_impl.rb
+++ cookbooks/bcpc-hadoop/recipes/zookeeper_impl.rb
@@ -74,5 +74,5 @@ service "zookeeper-server" do
   subscribes :restart, "link[/usr/lib/zookeeper/bin/zkServer.sh]", :delayed
   subscribes :restart, "file[#{node[:bcpc][:hadoop][:zookeeper][:data_dir]}/myid]", :delayed
   subscribes :restart, "user_ulimit[zookeeper]", :delayed
-  subscribes :restart, "bash[hdp-select zookeeper-server]", :immediate
+  subscribes :restart, "bash[hdp-select zookeeper-server]", :delayed
 end
